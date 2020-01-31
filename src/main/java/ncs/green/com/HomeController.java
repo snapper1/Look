3acf72c2e8fb811cdd19a07363021ef43460e99a
River_kim <khh3605@gmail.com>
2020-01-31 13:08:59 +0900

    코디 수정

--- src/main/java/ncs/green/com/HomeController.java
+++ src/main/java/ncs/green/com/HomeController.java
@@ -4,7 +4,6 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
