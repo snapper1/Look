4dfb5688a7cc4eadf0152254ea2d6a03f4cfef57
River_kim <khh3605@gmail.com>
2020-02-03 17:23:26 +0900

    codi화면 디테일 추가 ( hover , more 버튼 )

--- src/main/webapp/resources/js/codi.js
+++ src/main/webapp/resources/js/codi.js
@@ -1,4 +1,19 @@
-
-function More(){
-	alert('버튼을 맨 하단으로 이동+사진 띄우기');
-}
+$(document).ready(function(){
+				$('.aa').hide();
+				$('.section-two-divTwo-div').click(function() {
+					if($('.section-two-divTwo-div').hasClass('section-two-divTwo-div')){
+						$('.aa').show();
+						$('.section-two-divTwo-div').hide();
+					}else if($('.aa').show){
+						$('.section-two-divTwo-div').hide();
+					}
+				});
+			});
+$(document).ready(function(){
+	$('.menu').mouseover(function(){
+		$('.login-button').css("background-color","#FFFFFF");
+	});
+	$('.menu').mouseout(function(){
+		$('.login-button').css("background-color","#000000");
+	});
+});
